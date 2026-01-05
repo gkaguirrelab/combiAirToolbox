8cd347c3aebc6e95469ee632287970c7ae296513
gkaguirre <gkaguirre@me.com>
2026-01-05 15:54:57 -0500

    Update camera recording computer settings

--- code/@PuffCameraControl/PuffCameraControl.m
+++ code/@PuffCameraControl/PuffCameraControl.m
@@ -6,11 +6,11 @@ classdef PuffCameraControl < handle
     properties (Constant)
 
         USERNAME = 'gka';
-        HOSTNAME = '128.91.12.22';
+        HOSTNAME = '10.30.10.205';
         PASSWORD = 'braincrunch';
         rpiDataSaveStem = '/media/gka/EYEVIDEOS/';
         rpiCommandDir = '/home/gka/Documents/commands';
-        audioSourceCommand = 'pactl set-default-source alsa_input.pci-0000_07_00.6.analog-stereo'; % Use the external headphone jack source
+        audioSourceCommand = 'pactl set-default-source alsa_input.pci-0000_05_00.6.analog-stereo'; % Use the external headphone jack source
         usbResetCommand = 'usbreset 2560:c124'; %% This is the product and vendor ID for the right eye camera
         cameraSettingsCommand = 'v4l2-ctl -d /dev/video0 --set-ctrl=exposure_absolute=40 --set-ctrl=brightness=40';
         recordingCommandL = "ffmpeg -loglevel quiet -f v4l2 -input_format gray -video_size 640x480 -framerate 180 -thread_queue_size 4096 -i /dev/video0 -f alsa -ac 1 -ar 8000 -i default -t {dur} -vcodec rawvideo -pix_fmt gray -acodec pcm_s16le -y {stem}/{path}/{label}_side-L.avi &"
